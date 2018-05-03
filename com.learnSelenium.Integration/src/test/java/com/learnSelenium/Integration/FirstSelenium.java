acd2cea4f34f4a32483b1ea9827294850cbaae45
rathinamoorthy24 <rathinamoorthy24@gmail.com>
2018-05-02 23:46:32 -0700

    First commit

---
+++ com.learnSelenium.Integration/src/test/java/com/learnSelenium/Integration/FirstSelenium.java
@@ -0,0 +1,12 @@
+package com.learnSelenium.Integration;
+
+import org.testng.annotations.Test;
+
+public class FirstSelenium {
+
+	@Test
+	public void printHello()
+	{
+		System.out.println("Hello");
+	}
+}
